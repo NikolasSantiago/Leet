90e19a076a1889a64a4e2d043a67e6db2de002da
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-21 21:39:00 -0300

    finalizar programa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string fraseDigitada; 
+
+string fraseLeet;
+
+ Console.Clear();
+
+  Console.Write(" Digite uma frase: ");
+
+   fraseDigitada = Console.ReadLine()!;
+
+    fraseLeet = fraseDigitada 
+    .Replace("a", "4") 
+    .Replace("e", "3")
+    .Replace("i", "1")
+    .Replace("o", "0")
+    .Replace("t", "7")
+    .Replace("s", "5");
+
+     Console.WriteLine(fraseLeet);
